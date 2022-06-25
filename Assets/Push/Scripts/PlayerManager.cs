895ebea80898f8192321439edc6326342df2c9f1
Boyke01 <107166278+Boyke01@users.noreply.github.com>
2022-06-25 22:13:06 +0700

    Final Commit

---
+++ Assets/Push/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static int currentHealth = 500;
+    public Slider healthBar;
+    public static bool gameOver;
+
+    public GameObject gameOverPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = currentHealth;
+
+        if(currentHealth < 0)
+        {
+            gameOver = true;
+            gameOverPanel.SetActive(true);
+        }   
+    }
+}
